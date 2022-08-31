3519426e6b33a368428e1c9bdf7612589c79f5b9
enilson <enilsonsilva999@gmail.com>
2022-08-31 14:44:28 -0300

    feat: create toasty

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,15 @@ import "./App.css";
 import Routes from "./routes";
 import Global from "./styles/Global";
 import AuthProvider from "./contexts/AuthContext";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.min.css";
 
 function App() {
   return (
     <AuthProvider>
       <Global />
       <Routes />
+      <ToastContainer />
     </AuthProvider>
   );
 }
--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,5 +1,7 @@
-import { createContext, ReactNode } from "react";
-import { IUserLogin, loginUsers } from "../services/loginUserApi";
+import { createContext, ReactNode, useState } from "react";
+import api, { ICommonHeaderProperties } from "../services/api";
+import { IUserData, IUserLogin, loginUsers } from "../services/loginUserApi";
+import { toast } from "react-toastify";
 
 export interface IAuthContexProps {
   children: ReactNode;
@@ -7,20 +9,39 @@ export interface IAuthContexProps {
 
 interface IAuthContex {
   loginUser: (data: IUserLogin) => Promise<void>;
+  user: IUserData;
 }
 
 export const AuthContext = createContext<IAuthContex>({} as IAuthContex);
 
 const AuthProvider = ({ children }: IAuthContexProps) => {
+  const [user, setUser] = useState<IUserData>({} as IUserData);
+
   const loginUser = async (data: IUserLogin) => {
     loginUsers(data)
       .then((res) => {
         console.log(res);
+        const { user: userReponse, accessToken } = res;
+        api.defaults.headers = {
+          Authorization: `bearer ${accessToken}`,
+        } as ICommonHeaderProperties;
+        setUser(userReponse);
+        toast.success("Usuário logado com sucesso!", {
+          autoClose: 900,
+          theme: "dark",
+        });
+
+        localStorage.setItem("@AuqMia:token", accessToken);
       })
-      .catch((err) => console.log(err));
+      .catch((err) =>
+        toast.error("Senha ou email incorreto", {
+          autoClose: 900,
+          theme: "dark",
+        })
+      );
   };
   return (
-    <AuthContext.Provider value={{ loginUser }}>
+    <AuthContext.Provider value={{ loginUser, user }}>
       {children}
     </AuthContext.Provider>
   );
