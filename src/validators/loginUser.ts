31480acf813dc23c5e42b8425a866465871b9049
Gabriela Caldeira <gabihcaldeira@hotmail.com>
2022-08-31 22:58:03 -0300

    fix: required message and password min

--- src/validators/loginUser.ts
+++ src/validators/loginUser.ts
@@ -1,14 +1,11 @@
 import * as yup from "yup";
 
 const schema = yup.object({
-  email: yup
-    .string()
-    .email("Deve ser um email")
-    .required("Email é obrigatório"),
+  email: yup.string().email("Deve ser um email").required("Campo Obrigatório"),
   password: yup
     .string()
-    .required("Senha é obrigatória")
-    .min(6, "No mínimo 8 caracteres"),
+    .required("Campo Obrigatório")
+    .min(8, "No mínimo 8 caracteres"),
 });
 
 export default schema;
