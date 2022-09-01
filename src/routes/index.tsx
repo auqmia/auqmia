21de422553130efce7d42bf089fddb93240a0684
Gabriela Caldeira <gabihcaldeira@hotmail.com>
2022-08-31 22:58:24 -0300

    feat: add register route

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,12 +1,14 @@
 import { Route, Routes } from "react-router-dom";
 import Login from "../pages/Login";
 import Profile from "../pages/Profile";
+import Register from "../pages/Register";
 
 const RoutesMain = () => {
   return (
     <Routes>
       <Route path="/login" element={<Login />} />
       <Route path="/profile" element={<Profile />} />
+      <Route path="/register" element={<Register />} />
     </Routes>
   );
 };
