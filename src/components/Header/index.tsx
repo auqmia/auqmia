7df76c16b1784dd6004e2f3247ff079ba5b40280
Stevan <stevan14padilha@gmail.com>
2022-09-01 09:47:30 -0300

    fix: new styles

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,22 +1,28 @@
-import logoHeader from "../../assets/img/logoHeader.png";
-import auqMia from "../../assets/img/auqMia.png";
+import Logo from "../../assets/img/Logo.png";
+import LogoAuqMia from "../../assets/img/LogoAuqMia.png";
+import loginButton from "../../assets/img/ButtonsImg/loginButton.png";
+import donationButton from "../../assets/img/ButtonsImg/donationButton.png";
 import { HeaderMain } from "./styles";
 import { useContext } from "react";
 import { AuthContext } from "../../contexts/AuthContext";
 function Header() {
-    const {loginRoute} = useContext(AuthContext)
+  const { loginRoute } = useContext(AuthContext);
   return (
     <HeaderMain>
-      <div>
-        <img src={logoHeader} alt="logo" />
-        <img src={auqMia} alt="title" />
+      <div className="logo-div">
+        <img className="logo-AuqMia" src={LogoAuqMia} alt="LogoAuqMia" />
+        <img className="logo" src={Logo} alt="Logo" />
       </div>
       <div>
         <p>Mensagem que eu não sei!!</p>
       </div>
-      <div>
-        <button type="button" onClick={() => loginRoute()}>Login</button>
-        <button>Quero ajudar</button>
+      <div className="buttons-div">
+        <button className="login-button" type="button" onClick={() => loginRoute()}>
+          <img src={loginButton} alt="Login Button" />
+        </button>
+        <button className="donation-button">
+          <img src={donationButton} alt="Donation Button" />
+        </button>
       </div>
     </HeaderMain>
   );
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,7 +1,39 @@
 import styled from "styled-components";
 
 export const HeaderMain = styled.header`
-display: flex;
-justify-content: space-around;
-align-items: center;
-`
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+
+  box-shadow: 0px 5px 17px 4px rgba(31, 30, 30, 0.7);
+
+  .logo-div {
+    .logo-AuqMia {
+      display: none;
+    }
+
+    @media (min-width: 768px) {
+      .logo-AuqMia {
+        display: block;
+      }
+      .logo {
+        display: none;
+      }
+    }
+  }
+
+  .buttons-div {
+    display: flex;
+    flex-direction: column;
+
+    gap: 10px;
+
+    .login-button {
+      background: none;
+    }
+
+    .donation-button {
+      background: none;
+    }
+  }
+`;
