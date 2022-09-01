8122458238e8714214166892b518f5db72b4256c
douglasalves <douglas.estefanio@gmail.com>
2022-09-01 12:21:54 -0300

    fix: Dashboard finished

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,5 +1,6 @@
-import { createContext, ReactNode } from "react";
+import { createContext, ReactNode, useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { getAnimalsApi, IAnimals } from "../services/getAnimalsApi";
 import { IUserLogin, loginUsers } from "../services/loginUserApi";
 
 export interface IAuthContexProps {
@@ -9,12 +10,18 @@ export interface IAuthContexProps {
 interface IAuthContex {
   loginUser: (data: IUserLogin) => Promise<void>;
   loginRoute: () => void;
+  listAnimals: IAnimals[];
 }
 
 export const AuthContext = createContext<IAuthContex>({} as IAuthContex);
 
 const AuthProvider = ({ children }: IAuthContexProps) => {
   const navigate = useNavigate();
+  const [listAnimals, setListAnimals] = useState([]);
+
+  useEffect(() => {
+    getAnimals()
+  },[])
 
   const loginUser = async (data: IUserLogin) => {
     loginUsers(data)
@@ -28,8 +35,16 @@ const AuthProvider = ({ children }: IAuthContexProps) => {
     navigate("/login")
   }
 
+  function getAnimals() {
+    getAnimalsApi()
+    .then((res) => {
+      setListAnimals(res)
+    })
+    .catch((err) => console.log(err))
+  }
+
   return (
-    <AuthContext.Provider value={{ loginUser, loginRoute }}>
+    <AuthContext.Provider value={{ loginUser, loginRoute, listAnimals }}>
       {children}
     </AuthContext.Provider>
   );
