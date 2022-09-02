58bc0c0b2aee67e795e42459525f8db31f5a6b60
Stevan <stevan14padilha@gmail.com>
2022-09-02 10:57:01 -0300

    fix: dashboard bugs/state cards

--- src/components/Cards/index.tsx
+++ src/components/Cards/index.tsx
@@ -1,21 +1,59 @@
 import { useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
 import { AuthContext } from "../../contexts/AuthContext";
 import { IAnimals } from "../../services/getAnimalsApi";
+import { Ul } from "./style";
 
 function Cards() {
-  const { listAnimals } = useContext(AuthContext);
+  const navigate = useNavigate();
+
+  const { listAnimals, deleteAnimal, adopted, setAdopted } =
+    useContext(AuthContext);
+
+  const deleteCard = (id: string) => {
+    const token = localStorage.getItem("@AuqMia:token");
+
+    const list = document.querySelector(`.list${id}`);
+
+    if (token) {
+      setAdopted(!adopted);
+
+      list?.classList.add("remove-card");
+
+      toast.success("Solicitação de adoção enviada com sucesso!!", {
+        autoClose: 900,
+        theme: "dark",
+      });
+
+      /* setTimeout(() => deleteAnimal(id), 1000); */
+      deleteAnimal(id);
+    } else {
+      navigate("/login", { replace: true });
+      toast.error("Você precisa fazer o login para adotar.", {
+        autoClose: 900,
+        theme: "dark",
+      });
+    }
+  };
+
   return (
-    <div>
+    <Ul>
       {listAnimals.map((elem: IAnimals) => (
-        <div key={elem.id}>
-          <p>{elem.name}</p>
-          <img src={elem.url} alt="foto" />
-          <p>{elem.type}</p>
-          <p>{elem.genre}</p>
-          <p>{elem.description}</p>
-        </div>
+        <li key={elem.id} className={`list${elem.id}`}>
+          <h1>{elem.name}</h1>
+          <figure>
+            <img src={elem.url} alt="foto" />
+          </figure>
+          <div>
+            <h5>Animal: {elem.type}</h5>
+            <span>Gênero: {elem.genre}</span>
+            <p>Descrição: {elem.description}</p>
+          </div>
+          <button onClick={() => deleteCard(elem.id)}>Adotar</button>
+        </li>
       ))}
-    </div>
+    </Ul>
   );
 }
 
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -2,54 +2,122 @@ import styled from "styled-components";
 
 export const HeaderMain = styled.header`
   display: flex;
-  justify-content: space-around;
-  align-items: center;
+  justify-content: center;
+
+  animation: fadeInLeft 1s;
+
+  padding: 0px 40px;
 
   box-shadow: 0px 5px 17px 4px rgba(31, 30, 30, 0.7);
 
-  .logo-div {
-    .logo-AuqMia {
-      display: none;
+  margin-bottom: 40px;
+
+  section {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+
+    width: 100%;
+    max-width: 1290px;
+
+    .logo-div {
+      .logo {
+        height: 120px;
+      }
+
+      .logo-AuqMia {
+        display: none;
+
+        height: 120px;
+      }
+
+      @media (min-width: 768px) {
+        .logo-AuqMia {
+          display: block;
+        }
+        .logo {
+          display: none;
+        }
+      }
     }
 
-    @media (min-width: 768px) {
-      .logo-AuqMia {
-        display: block;
-      }
-      .logo {
+    .mid-div {
+      display: flex;
+
+      h1 {
         display: none;
       }
-    }
-  }
+      @media (min-width: 768px) {
+        h1 {
+          display: flex;
+          text-align: center;
 
-  .buttons-div {
-    display: flex;
-    flex-direction: column;
+          width: 300px;
 
-    gap: 10px;
+          font-size: 1.5rem;
 
-    .login-button {
-      background: none;
-    }
-    .login-button:hover {
-      animation: pulse 1s;
-    }
-    .new-login-button {
-      background: none;
+          color: var(--gray-0);
+          /* text-shadow: 0.1em 0.1em 0.2em var(--color-primary); */
+        }
+      }
 
-      animation: backInRight 1s;
+      .footprints-figure {
+        display: none;
+
+        max-width: 200px;
+        max-height: 120px;
+      }
+      .footprints-img {
+        height: 100%;
+        width: 100%;
+        object-fit: contain;
+      }
+
+      @media (min-width: 476px) {
+        .footprints-figure {
+          display: block;
+        }
+      }
     }
 
-    .donation-button {
-      background: none;
-    }
-    .donation-button:hover {
-      animation: pulse 1s;
-    }
-    .new-donation-button {
-      background: none;
+    .buttons-div {
+      display: flex;
+      flex-direction: column;
+      justify-content: space-between;
 
-      animation: backInRight 1s;
+      gap: 10px;
+
+      .login-button {
+        background: none;
+      }
+      .login-button:hover {
+        animation: pulse 1s;
+      }
+      .login-button-img {
+        height: 40px;
+      }
+
+      .new-login-button {
+        background: none;
+
+        animation: backInRight 1s;
+      }
+
+      .donation-button {
+        background: none;
+      }
+      .donation-button:hover {
+        animation: pulse 1s;
+      }
+      .donation-button-img {
+        height: 40px;
+      }
+
+      .new-donation-button {
+        background: none;
+
+        animation: backInRight 1s;
+      }
     }
   }
 `;
