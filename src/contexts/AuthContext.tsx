ca1e2d593d384705264075d1a19a30563837e22c
Gabriela Caldeira <gabihcaldeira@hotmail.com>
2022-09-02 12:46:49 -0300

    feat: add register function

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -3,25 +3,27 @@ import api, { ICommonHeaderProperties } from "../services/api";
 import { IUserData, IUserLogin, loginUsers } from "../services/loginUserApi";
 import { toast } from "react-toastify";
 import { useNavigate } from "react-router-dom";
+import { IUserRegister } from "../services/registerUserApi";
 
-export interface IAuthContexProps {
+export interface IAuthContextProps {
   children: ReactNode;
 }
 
-interface IAuthContex {
+interface IAuthContext {
   loginUser: (data: IUserLogin) => Promise<void>;
   user: IUserData;
   isLogged: boolean;
   loading: boolean;
+  registerUser: (data: IUserRegister) => void;
 }
 
-export const AuthContext = createContext<IAuthContex>({} as IAuthContex);
+export const AuthContext = createContext<IAuthContext>({} as IAuthContext);
 
-const AuthProvider = ({ children }: IAuthContexProps) => {
+const AuthProvider = ({ children }: IAuthContextProps) => {
   const [user, setUser] = useState<IUserData>({} as IUserData);
   const [loading, setLoading] = useState(true);
   const [isLogged, setIsLogged] = useState(false);
-  const navegate = useNavigate();
+  const navigate = useNavigate();
 
   useEffect(() => {
     const loadUser = async () => {
@@ -54,18 +56,41 @@ const AuthProvider = ({ children }: IAuthContexProps) => {
           autoClose: 900,
           theme: "dark",
         });
-        navegate("/profile", { replace: true });
+        navigate("/profile", { replace: true });
         localStorage.setItem("@AuqMia:token", accessToken);
       })
       .catch((err) =>
-        toast.error("Senha ou email incorreto", {
+        toast.error("Senha ou email incorreto.", {
           autoClose: 900,
           theme: "dark",
         })
       );
   };
+
+  const registerUser = (data: IUserRegister) => {
+    const { confirm_password, ...userData } = data;
+    api
+      .post("/register", userData)
+      .then((res) => {
+        toast.success("Usuário registrado com sucesso!", {
+          autoClose: 900,
+          theme: "dark",
+        });
+        navigate("/login", { replace: true });
+      })
+      .catch((err) => {
+        console.log(err);
+        toast.error("Não foi possível fazer o cadastro.", {
+          autoClose: 900,
+          theme: "dark",
+        });
+      });
+  };
+
   return (
-    <AuthContext.Provider value={{ loginUser, user, isLogged, loading }}>
+    <AuthContext.Provider
+      value={{ loginUser, user, isLogged, loading, registerUser }}
+    >
       {children}
     </AuthContext.Provider>
   );
