4ef28bf0a2a44479399646170d66e34b6c47a0f0
Gabriela Caldeira <gabihcaldeira@hotmail.com>
2022-09-06 00:28:53 -0300

    feat: create profile types

--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -1,12 +1,8 @@
 import { useContext, useState } from "react";
 import { Navigate } from "react-router-dom";
 import { AuthContext } from "../../contexts/AuthContext";
-import { MdOutlineEditNote } from "react-icons/md";
-import { HeaderProfile, DivMain } from "./stelys";
-import { RiAddFill } from "react-icons/ri";
-import { Ul } from "../../components/Cards/style";
+import { HeaderProfile } from "./style";
 import logoAqMia from "../../assets/img/LogoAuqMia.png";
-import Cards from "../../components/Cards";
 import logout from "../../assets/logout.svg";
 import donate from "../../assets/donate.svg";
 import logo from "../../assets/img/Logo.png";
@@ -14,10 +10,11 @@ import ModalUpdateRegister from "../../components/ModalUpUser";
 
 import RegisterPets from "../../components/modalRegisterPets";
 import RegisterSupplies from "../../components/modalRegisterSupplies";
+import ProfileForAdoption from "../ProfileForAdoption";
+import ProfileAdoptPet from "../ProfileAdoptPet";
 
 const Profile = () => {
-  const { loading, isLogged, user, backProfile, setModalUpdateUser } =
-    useContext(AuthContext);
+  const { loading, isLogged, user, backProfile } = useContext(AuthContext);
 
   const [isActiveModalPet, setIsActiveModalPet] = useState(false);
   const [isActiveModalSupplies, setIsActiveModalSupplies] = useState(false);
@@ -45,96 +42,28 @@ const Profile = () => {
           </div>
         </div>
       </HeaderProfile>
-      <DivMain>
-        <div className="div-wrap">
-          <main className="main-profile">
-            <div className="subHeader-title">
-              <h1>Minha raça favorita é: adotada.</h1>
-            </div>
-            <div className="div-container-info">
-              <div className="div-info">
-                <figure>
-                  <img
-                    className="img-profile"
-                    src={user.url}
-                    alt="img profile"
-                  />
-                </figure>
-                <div className="div-name-location">
-                  <h2 className="name-user">{user.name}</h2>
-                </div>
-              </div>
 
-              <div className="info">
-                <p>Email:</p>
-                <p>Cidade:</p>
-                <p>Estado:</p>
-                <p>Cidade:</p>
-              </div>
-              <div className="data-user">
-                <p className="name-user">
-                  "Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                  Vivamus lobortis id eros at auctor. Praesent in nibh et sapien
-                  luctus consectetur. Pellentesque efficitur sapien lectus,
-                  posuere laoreet urna sagittis in. Sed placerat dignissim
-                  tortor nec gravida."
-                </p>
-                {/*   <div className="div-email">
-                  <p className="name-user">{user.email}</p>
-                  <MdOutlineEditNote className="icon-edit" />
-                </div> */}
-                <p className="name-user">
-                  "Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                  Vivamus lobortis id eros at auctor. Praesent in nibh et sapien
-                  luctus consectetur. Pellentesque efficitur sapien lectus,
-                  posuere laoreet urna sagittis in. Sed placerat dignissim
-                  tortor nec."
-                </p>
-              </div>
-              <div className="div-email">
-                <p className="name-user">{user.email}</p>
-                <button onClick={() => setModalUpdateUser(true)}>
-                  <MdOutlineEditNote className="icon-edit" />
-                </button>
-              </div>
-            </div>
-
-            <div className="container-pets">
-              <div className="div-more-pets">
-                <button
-                  onClick={() => setIsActiveModalPet(!isActiveModalPet)}
-                  className="button-more-pets"
-                >
-                  <RiAddFill className="icon-button" />
-                </button>
-                <h3 className="title-more-pets">Colocar para Adoção</h3>
-              </div>
-              <div className="container-ul">
-                <Ul className="ul-profile">
-                  <Cards />
-                </Ul>
-              </div>
-              <div className="div-more-pets">
-                <button className="button-requests" onClick={() => setIsActiveModalSupplies(!isActiveModalSupplies)}>
-                  <RiAddFill className="icon-button" />
-                </button>
-                <p className="need-help">Preciso de Ajuda</p>
-              </div>
-            </div>
-          </main>
-        </div>
-      </DivMain>
+      {user.user_type === "forAdoption" ? (
+        <ProfileForAdoption />
+      ) : (
+        <ProfileAdoptPet />
+      )}
 
       {isActiveModalPet && (
-        <RegisterPets isActive={isActiveModalPet} setIsActive={setIsActiveModalPet} />
+        <RegisterPets
+          isActive={isActiveModalPet}
+          setIsActive={setIsActiveModalPet}
+        />
       )}
 
       {isActiveModalSupplies && (
-        <RegisterSupplies isActive={isActiveModalSupplies} setIsActive={setIsActiveModalSupplies} />
+        <RegisterSupplies
+          isActive={isActiveModalSupplies}
+          setIsActive={setIsActiveModalSupplies}
+        />
       )}
 
       <ModalUpdateRegister />
-
     </>
   ) : (
     <Navigate to="/login" replace />
