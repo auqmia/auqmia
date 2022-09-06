4b23867d393cc587f10ad4167a5cf3f9e23d8da2
Rafael <rafaelskz92@gmail.com>
2022-09-06 10:19:36 -0300

    feat: Page Donate, Modal Donate and Route Donate

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,6 +3,7 @@ import Dashboard from "../pages/Dashboard";
 import Login from "../pages/Login";
 import Profile from "../pages/Profile";
 import Register from "../pages/Register";
+import Donate from "../pages/Donate";
 
 const RoutesMain = () => {
   return (
@@ -11,6 +12,7 @@ const RoutesMain = () => {
       <Route path="/login" element={<Login />} />
       <Route path="/profile" element={<Profile />} />
       <Route path="/register" element={<Register />} />
+      <Route path="/donate" element={<Donate />} />
     </Routes>
   );
 };
