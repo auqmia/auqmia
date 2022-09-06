c7ca41b52982f47b7dfa9996cb435602786397c7
Gabriela Caldeira <gabihcaldeira@hotmail.com>
2022-09-06 20:39:41 -0300

    style: add style ProfileAdoptPet

--- src/pages/ProfileAdoptPet/index.tsx
+++ src/pages/ProfileAdoptPet/index.tsx
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../../contexts/AuthContext";
 import { BiHomeHeart } from "react-icons/bi";
 import { MdOutlineEditNote } from "react-icons/md";
@@ -8,20 +8,29 @@ import { Link } from "react-router-dom";
 const ProfileAdoptPet = () => {
   const { user, setModalUpdateUser } = useContext(AuthContext);
   const { name, picture, email, birthday, address, bio } = user;
-
+  const [birthDate] = useState(() => {
+    const date = new Date(birthday);
+    const month = date.getMonth() + 1;
+    return `${date.getDate()}/${
+      month < 10 ? `0${month}` : month
+    }/${date.getFullYear()}`;
+  });
+  useEffect(() => {
+    console.log(birthDate);
+  });
   return (
     <Main>
       <div className="mobile__title">
-        <h1>Minha raça favorita é: adotada.</h1>
+        <h1>"Minha raça favorita é: adotada.</h1>
       </div>
 
-      <div className="user__info">
-        <div className="info__main">
-          <figure className="info__figure">
-            <img className="info__img" src={picture} alt={`${name}`} />
+      <section className="section__left">
+        <div className="user__mainInfo">
+          <figure className="user__figure">
+            <img src={picture} alt={`${name}`} />
           </figure>
 
-          <div className="info__name">
+          <div className="user__name">
             <h2>{name}</h2>
             <p>
               {address?.city}, {address?.state}
@@ -29,41 +38,53 @@ const ProfileAdoptPet = () => {
           </div>
         </div>
 
-        <div className="info__more">
-          <MdOutlineEditNote />
-          <p>Email: {email}</p>
-          <p>Data de nascimento: {birthday}</p>
-          <p>Estado: {address?.state}</p>
-          <p>Cidade: {address?.city}</p>
-          <p>Bairro: {address?.district}</p>
+        <div className="user__moreInfo">
+          <MdOutlineEditNote className="icon__edit" />
+          <p>
+            Email: <span> {email}</span>
+          </p>
+          <p>
+            Data de nascimento: <span>{birthDate}</span>
+          </p>
+          <p>
+            Estado: <span>{address?.state}</span>
+          </p>
+          <p>
+            Cidade: <span>{address?.city}</span>
+          </p>
+          <p>
+            Bairro: <span>{address?.district}</span>
+          </p>
         </div>
-      </div>
 
-      <div className="container">
-        <div className="container__bio">
+        <div className="donate__link">
+          <p>
+            Você também pode
+            <Link to="/donations" className="link--donate">
+              &nbsp;doar&nbsp;
+            </Link>
+            suprimentos para quem precisa.
+          </p>
+        </div>
+      </section>
+
+      <section className="section__right">
+        <div className="user__bio">
           <p>{bio}</p>
         </div>
 
-        <div className="container__links">
-          <div className="">
-            <Link to="/" className="link--adopt">
+        <div className="container__pets">
+          <div className="pets__header">
+            <Link to="/" className="pets__link">
               <BiHomeHeart />
             </Link>
 
-            <p>Procure um pet para adotar!</p>
+            <p className="pets__text">Procure um pet para adotar!</p>
           </div>
 
-          <div>
-            <p>
-              Você também pode
-              <Link to="/donations" className="link--donate">
-                doar
-              </Link>
-              suprimentos para quem precisa.
-            </p>
-          </div>
+          <div className="pets__cards">Exibir fotos de pets!</div>
         </div>
-      </div>
+      </section>
     </Main>
   );
 };
--- src/pages/ProfileAdoptPet/style.ts
+++ src/pages/ProfileAdoptPet/style.ts
@@ -1,3 +1,107 @@
 import styled from "styled-components";
 
-export const Main = styled.main``;
+export const Main = styled.main`
+  width: 95%;
+  margin: 30px auto;
+  color: var(--gray-0);
+  font-family: var(--font-family);
+
+  .mobile__title {
+    font-size: 18px;
+    font-style: italic;
+    text-align: center;
+    margin-bottom: 50px;
+
+    > h1 {
+      font-weight: 400;
+    }
+  }
+
+  .section__left {
+    > .user__mainInfo {
+      width: 100%;
+      display: flex;
+      align-items: flex-end;
+
+      > .user__figure {
+        > img {
+          width: 100px;
+          height: 100px;
+          object-fit: cover;
+          border-radius: 100px;
+        }
+      }
+
+      > .user__name {
+        width: fit-content;
+        margin-left: 15px;
+        color: var(--color-secondary);
+        font-size: 18px;
+        font-weight: 200;
+        > h2 {
+          font-size: 20px;
+          font-weight: 500;
+        }
+      }
+    }
+
+    > .user__moreInfo {
+      width: 100%;
+      margin: 30px auto;
+      padding: 20px 15px 10px;
+      background-color: var(--gray-2);
+      border-radius: 10px;
+      position: relative;
+
+      > .icon__edit {
+        position: absolute;
+        font-size: 18px;
+        color: var(--color-third);
+        top: 5px;
+        right: 10px;
+      }
+
+      > p {
+        color: var(--color-third);
+        font-weight: 600;
+        > span {
+          color: var(--gray-0);
+          font-weight: 300;
+        }
+      }
+    }
+
+    > .donate__link {
+      font-size: 22px;
+      text-align: center;
+      margin-bottom: 40px;
+
+      > p {
+        font-weight: 200;
+
+        > a.link--donate {
+          cursor: pointer;
+          text-decoration: none;
+          color: var(--color-primary);
+          font-style: italic;
+          font-weight: 700;
+        }
+      }
+    }
+  }
+
+  .section__right {
+    > .user__bio {
+    }
+    > .container__pets {
+      > .pets__header {
+        > .pets__link {
+        }
+        > .pets__text {
+        }
+      }
+      > .pets__cards {
+      }
+    }
+  }
+`;
