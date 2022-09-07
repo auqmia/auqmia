0a279aecc80d50dd20e4166d6b423dd0ac368e8a
Gabriela Caldeira <gabihcaldeira@hotmail.com>
2022-09-06 22:46:18 -0300

    feat: add get 2 pets

--- src/services/getAnimalsApi.ts
+++ src/services/getAnimalsApi.ts
@@ -14,3 +14,8 @@ export async function getAnimalsApi(): Promise<IAnimals[]> {
 
   return data;
 }
+
+export async function getThreeAnimals(): Promise<IAnimals[]> {
+  const { data } = await api.get("/animals?_limit=2");
+  return data;
+}
