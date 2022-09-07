244e76c71ac71e0ba278860217da5425a30a9373
Gabriela Caldeira <gabihcaldeira@hotmail.com>
2022-09-07 18:25:04 -0300

    feat: add location filter

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,11 +1,13 @@
 import Cards from "../../components/Cards";
 import { Ul } from "../../components/Cards/style";
 import Header from "../../components/Header";
+import LocationFilter from "../../components/LocationFilter";
 
 function Dashboard() {
   return (
     <>
       <Header />
+      <LocationFilter />
       <Ul>
         <Cards />
       </Ul>
