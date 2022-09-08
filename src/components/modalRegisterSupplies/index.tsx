2bd00f4326216f7128fe3a0d3f4336a8da199442
Gabriela Caldeira <gabihcaldeira@hotmail.com>
2022-09-08 00:39:59 -0300

    fix: spelling erros

--- src/components/modalRegisterSupplies/index.tsx
+++ src/components/modalRegisterSupplies/index.tsx
@@ -19,7 +19,8 @@ const RegisterSupplies = () => {
   const [quantity, setQuantity] = useState("");
   const [userData, setUserData] = useState({});
 
-  const { isOpenModalSupplies, setIsOpenModalSupplis } = useContext(AuthContext);
+  const { isOpenModalSupplies, setIsOpenModalSupplies } =
+    useContext(AuthContext);
 
   const productList = [
     "Areia Higiênica",
@@ -28,28 +29,28 @@ const RegisterSupplies = () => {
   ];
   const quantityList = ["2kg", "5kg", "10kg", "20kg", "30kg"];
 
-  useEffect( () => {
+  useEffect(() => {
     if (token) {
       api
         .get(`/users/${id}`, {
           headers: { Authorization: `Bearer ${token}` },
         })
-        .then(res => setUserData(res.data))
+        .then((res) => setUserData(res.data));
     }
-  })
+  });
 
-  const req = { userID: id, product, quantity, userData: userData };
+  const req = { userId: id, product, quantity, userData: userData };
 
   function handleSubmit(event: any) {
     event.preventDefault();
 
-    if (token) {    
+    if (token) {
       api
         .post("/supplies", req, {
           headers: { Authorization: `Bearer ${token}` },
         })
         .then((res) => {
-          setIsOpenModalSupplis(!isOpenModalSupplies);
+          setIsOpenModalSupplies(!isOpenModalSupplies);
           toast.success("Suplemento cadastrado com sucesso!", {
             autoClose: 900,
             theme: "dark",
@@ -72,18 +73,20 @@ const RegisterSupplies = () => {
         <form className="supplies__form" onSubmit={handleSubmit}>
           <Select
             label="Produto"
+            showLabel={true}
             options={productList}
             onClick={(e) => setProduct(e.target.id)}
           />
           <Select
             label="Quantidade"
+            showLabel={true}
             options={quantityList}
             onClick={(e) => setQuantity(e.target.id)}
           />
           <DivButton className="div-button">
             <ButtonBack
               type="button"
-              onClick={() => setIsOpenModalSupplis(!isOpenModalSupplies)}
+              onClick={() => setIsOpenModalSupplies(!isOpenModalSupplies)}
             >
               <MdArrowBack className="icon__arrow" />
             </ButtonBack>
